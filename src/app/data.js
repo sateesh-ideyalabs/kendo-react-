1b817b10767866a4a54aa6ccaa17f6a05cfe5fc2
Sateesh Turlapati <venkatat@netapp.com>
2020-10-10 23:01:15 -0700

    Drag & Drop added images

--- src/app/data.js
+++ src/app/data.js
@@ -3,7 +3,7 @@ const data = [
         taskID: 1,
         ownerId: 1,
         title: "Fast and furious 6",
-        image: "https://www.imdb.com/title/tt1905041/mediaviewer/rm3465715968?ref_=tt_ov_i",
+        image: "../images/tt1905041.jpg",
         imdb: "http://www.imdb.com/title/tt1905041/",
         start: new Date("2013/6/13 17:00"),
         end: new Date("2013/6/13 18:30")
@@ -12,7 +12,7 @@ const data = [
         taskID: 2,
         ownerId: 3,
         title: "The Internship",
-        image: "https://www.imdb.com/title/tt2234155/mediaviewer/rm2225251328?ref_=tt_ov_i",
+        image: "../images/tt2234155.jpg",
         imdb: "http://www.imdb.com/title/tt2234155/",
         start: new Date("2013/6/13 14:00"),
         end: new Date("2013/6/13 15:30")
@@ -21,7 +21,7 @@ const data = [
         taskID: 3,
         ownerId: 2,
         title: "The Perks of Being a Wallflower",
-        image: "https://www.imdb.com/title/tt1659337/mediaviewer/rm2134748161?ref_=tt_ov_i",
+        image: "../images/tt1659337.jpg",
         imdb: "http://www.imdb.com/title/tt1659337/",
         start: new Date("2013/6/13 16:00"),
         end: new Date("2013/6/13 17:30")
@@ -30,7 +30,7 @@ const data = [
         taskID: 4,
         ownerId: 1,
         title: "The Help",
-        image: "https://www.imdb.com/title/tt1454029/mediaviewer/rm925546496?ref_=tt_ov_i",
+        image: "../images/tt1454029.jpg",
         imdb: "http://www.imdb.com/title/tt1454029/",
         start: new Date("2013/6/13 12:00"),
         end: new Date("2013/6/13 13:30")
@@ -39,7 +39,7 @@ const data = [
         taskID: 5,
         ownerId: 2,
         title: "Now You See Me",
-        image: "https://www.imdb.com/title/tt1670345/mediaviewer/rm1351393536?ref_=tt_ov_i",
+        image: "../images/tt1670345.jpg",
         imdb: "http://www.imdb.com/title/tt1670345/",
         start: new Date("2013/6/13 10:00"),
         end: new Date("2013/6/13 11:30")
@@ -48,7 +48,7 @@ const data = [
         taskID: 6,
         ownerId: 1,
         title: "Fast and furious 6",
-        image: "https://www.imdb.com/title/tt1905041/mediaviewer/rm3465715968?ref_=tt_ov_i",
+        image: "../images/tt1905041.jpg",
         imdb: "http://www.imdb.com/title/tt1905041/",
         start: new Date("2013/6/13 19:00"),
         end: new Date("2013/6/13 20:30")
@@ -57,7 +57,7 @@ const data = [
         taskID: 7,
         ownerId: 3,
         title: "The Internship",
-        image: "https://www.imdb.com/title/tt2234155/mediaviewer/rm2225251328?ref_=tt_ov_i",
+        image: "../images/tt2234155.jpg",
         imdb: "http://www.imdb.com/title/tt2234155/",
         start: new Date("2013/6/13 17:30"),
         end: new Date("2013/6/13 19:00")
@@ -66,7 +66,7 @@ const data = [
         taskID: 8,
         ownerId: 1,
         title: "The Perks of Being a Wallflower",
-        image: "https://www.imdb.com/title/tt1659337/mediaviewer/rm2134748161?ref_=tt_ov_i",
+        image: "../images/tt1659337.jpg",
         imdb: "http://www.imdb.com/title/tt1659337/",
         start: new Date("2013/6/13 17:30"),
         end: new Date("2013/6/13 19:00")
@@ -75,7 +75,7 @@ const data = [
         taskID: 9,
         ownerId: 2,
         title: "The Help",
-        image: "https://www.imdb.com/title/tt1454029/mediaviewer/rm925546496?ref_=tt_ov_i",
+        image: "../images/tt1454029.jpg",
         imdb: "http://www.imdb.com/title/tt1454029/",
         start: new Date("2013/6/13 13:30"),
         end: new Date("2013/6/13 15:00")
@@ -84,7 +84,7 @@ const data = [
         taskID: 10,
         ownerId: 2,
         title: "Now You See Me",
-        image: "https://www.imdb.com/title/tt1670345/mediaviewer/rm1351393536?ref_=tt_ov_i",
+        image: "../images/tt1670345.jpg",
         imdb: "http://www.imdb.com/title/tt1670345/",
         start: new Date("2013/6/13 12:30"),
         end: new Date("2013/6/13 14:00")
